e1bf4871ad128533a6bfb4e0249d035a909dd548
--global <priyanshuyadav12@gmail.com>
2023-03-15 06:10:59 +0530

    updated bucket policies

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-001122-22"
-  location = "us-central1"
-  uniform_bucket_level_access = true
+  name                          = "test-bucket-random-001122-22"
+  location                      = "us-central1"
+  uniform_bucket_level_access   = true
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-random-00112323"
-  location = "us-central1"
-  uniform_bucket_level_access = true
+  name                          = "test-bucket-random-00112323"
+  location                      = "us-central1"
+  uniform_bucket_level_access   = true
 }
